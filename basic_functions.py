4bd7a84d1ab99b53fe870ab2a84ef3f5031e9cbe
zexindai <zexin.dai@gmail.com>
2020-12-10 21:27:16 +0100

    Basic functions used

---
+++ basic_functions.py
@@ -0,0 +1,19 @@
+import time
+import os
+
+def download_wait(directory, timeout, nfiles=None):
+    seconds = 0
+    dl_wait = True
+    while dl_wait and seconds < timeout:
+        time.sleep(1)
+        dl_wait = False
+        files = os.listdir(directory)
+        if nfiles and len(files) != nfiles:
+            dl_wait = True
+
+        for fname in files:
+            if fname.endswith('.crdownload'):
+                dl_wait = True
+
+        seconds += 1
+    return seconds
